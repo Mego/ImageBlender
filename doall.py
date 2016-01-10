52576207c2a50b08d6b367488ce60a76f42ec59e
Ryan Rowe <Mego@users.noreply.github.com>
2016-01-10 01:02:33 -0600

    fix doall

--- doall.py
+++ doall.py
@@ -4,4 +4,4 @@ import os
 
 for fname in os.listdir('.'):
     if fname.endswith('.png') and not fname.endswith('_.png'):
-        os.system('pngtopnm {0} -plain > {1} && ./imagemoment.py {1} > {2} && pnmtopng {2} > {3}'.format(fname, fname[:-4]+'.pnm', fname[:-4]+'_.pnm', fname[:-4]+'_.png'))
+        os.system('pngtopnm {0} -plain > {1} && ./imageblender.py {1} > {2} && pnmtopng {2} > {3}'.format(fname, fname[:-4]+'.pnm', fname[:-4]+'_.pnm', fname[:-4]+'_.png'))
