532c819fc4c414ec66c1f7f19a29d2efa4a4289b
Ryan Rowe <Mego@users.noreply.github.com>
2016-01-10 02:33:41 -0600

    fix bug

--- imageblender.py
+++ imageblender.py
@@ -5,14 +5,10 @@ from itertools import *
 
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
-    return izip_longest(*args, fillvalue=fillvalue)
+    return list(izip_longest(*args, fillvalue=fillvalue))
 
-def rgb_to_bin(r,g,b):
-    return int(hex(r)[2:] + hex(g)[2:] + hex(b)[2:], 16)
-
-def bin_to_rgb(b):
-    h = '%06X'%(b)
-    return map(lambda x:int(''.join(x), 16), grouper(h, 2))
+def flatten(lst):
+    return sum(([x] if not isinstance(x, list) else flatten(x) for x in lst), [])
 
 def pnm_to_bin(p):
     w,h = map(int,p[1].split(' '))
@@ -22,14 +18,14 @@ def pnm_to_bin(p):
     for line in lines:
         data = []
         for rgb in grouper(line, 3):
-            data.append(rgb_to_bin(*rgb))
+            data.append(list(rgb))
         bin.append(data)
     return bin
 
 def bin_to_pnm(b):
     pnm = 'P3 {} {} 255 '.format(len(b[0]), len(b))
-    b = [z for a in b for z in a]
-    pnm += ' '.join(map(lambda x:' '.join(map(str, bin_to_rgb(x))), b))
+    b = flatten(b)
+    pnm += ' '.join(map(str, b))
     return pnm
 
 def imageblender(img):
@@ -37,8 +33,9 @@ def imageblender(img):
     w = len(img[0])
     for y in range(w):
         for x in range(h):
-            val = (img[x][y] + img[x][~y] + img[~x][y] + img[~x][~y])//4
-            img[x][y],img[x][~y],img[~x][y],img[~x][~y] = (val,)*4
+            for i in range(3):
+                val = (img[x][y][i] + img[x][~y][i] + img[~x][y][i] + img[~x][~y][i])//4
+                img[x][y][i],img[x][~y][i],img[~x][y][i],img[~x][~y][i] = (val,)*4
     return img
 
 def main(fname):
